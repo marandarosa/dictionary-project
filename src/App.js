81dca5a8bcb3afd044d09cf77b0b0698ffe117bc
marandarosa <marandavalentin@aol.com>
2024-02-13 07:42:52 -0500

    Created initial header

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1 className="mt-5">Dictionary</h1>
+      <header className="App-header container">
+        <h1>Dictionary</h1>
       </header>
     </div>
   );
